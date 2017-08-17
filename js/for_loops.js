44154eba70a084efdbda2d8a3c2940e3c3f0fd8e
zacharyramos <zachary.ramos9@gmail.com>
2017-08-17 16:36:38 -0500

    * created break_and_continue js

--- js/for_loops.js
+++ js/for_loops.js
@@ -7,11 +7,48 @@
 
 
 
-for (var i = 1; i <= 10; i++ ) {
-    var randomNumber = Math.floor(Math.random() * 180) + 20;
-    if (randomNumber % 2 !== 0) {
-        console.log(randomNumber + " is odd");
-    } else {
-        console.log(randomNumber + " is even");
-    }
-}
+// for (var i = 1; i <= 10; i++ ) {
+//     var randomNumber = Math.floor(Math.random() * 180) + 20;
+//     if (randomNumber % 2 !== 0) {
+//         console.log(randomNumber + " is odd");
+//     } else {
+//         console.log(randomNumber + " is even");
+//     }
+// }
+
+// var i;
+//
+// for (i = 1; i <= 10; i++) {
+//     //    if i = 1 - 9 then log that #. Else log 0
+//
+//
+//
+//     var a = '';
+//     for (var n = 1; n <= i; n++) {
+//
+//         if(i === 10) {
+//             var a=a+''+ (i * 0);
+//         }else {
+//             var a=a+''+i;
+//         }
+//
+//     }
+//         console.log(a);
+//
+// }
+
+// for(i = 1; i <= 10; i++) {
+//     console.log((i % 10).toString().repeat(i));
+// }
+
+
+// var i;
+//
+//
+// for(i = 1; i <= 20; i++) {
+//     var n = 5;
+//     if()
+//
+// }
+//
+// console.log(n)
