5c3fe0e1fec31c15e965837978c9f61fabc8dde9
zacharyramos <zachary.ramos9@gmail.com>
2017-08-25 11:58:10 -0500

    created and completed inline and external js exercises

---
+++ js/external.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+var favColor = prompt("what is your favorite color?");
+alert(favColor + " is my favorite too!");
